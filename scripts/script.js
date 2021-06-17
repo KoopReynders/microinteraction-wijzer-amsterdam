d2068ff692060aa70617c5d5ea76c41b7a5c668f
Joost Faber <j.faber@hva.nl>
2021-06-17 11:39:44 +0200

    added flip animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,24 @@
-const main = document.querySelector('main');
+const main = document.querySelector('main')
 const chat = document.querySelector('#chat iframe')
-const observer = new IntersectionObserver(showChat);
+const card = document.querySelector('div.card')
+const buttons = document.querySelectorAll('div.card button')
+
+const observer = new IntersectionObserver(showChat)
 
 observer.observe(chat) 
+
+buttons.forEach(button => button.addEventListener('click', flip))
 
 function showChat(entries, observer) { 
   entries.forEach(entry => {
     if(entry.isIntersecting) {
-        console.log(1)
       main.classList.add('observed')
     } else {
       main.classList.remove('observed')
     }
   });
 };
+
+function flip () {
+  card.classList.toggle('flipped')
+}
